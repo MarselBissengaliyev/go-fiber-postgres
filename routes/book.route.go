8625b1d2aa65f97fa0bd67efff5a44ba149a42f2
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2023-09-09 20:55:23 +0500

    Update: book.route.go

--- routes/book.route.go
+++ routes/book.route.go
@@ -1,8 +1,16 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/MarselBissengaliyev/go-fiber-postgres/repositories"
+	"github.com/gofiber/fiber/v2"
+)
 
 func RegisterBookStoreRoutes(rg fiber.Router) {
+	r := repositories.Repository{}
+
 	routes := rg.Group("/books")
-	routes.Get("/")
+	routes.Get("/", r.GetBooks)
+	routes.Get("/:id", r.GetBookById)
+	routes.Post("/", r.CreateBook)
+	routes.Delete("/:id", r.DeleteBookById)
 }
