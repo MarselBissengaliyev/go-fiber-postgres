442373866aeff8c4e06d38cacfef7f8055e4022d
Marsel Bissengaliyev <marselbisengaliev1@gmail.com>
2023-09-09 19:15:32 +0500

    Update: book.model.go

--- models/book.model.go
+++ models/book.model.go
@@ -1,7 +1,17 @@
 package models
 
+import (
+	"gorm.io/gorm"
+)
+
 type Book struct {
-	Author    string `json:"author"`
-	Title     string `json:"title"`
-	Publisher string `json:"publisher"`
+	gorm.Model
+	Author    *string `json:"author"`
+	Title     *string `json:"title"`
+	Publisher *string `json:"publisher"`
+}
+
+func MigrateBooks(db *gorm.DB) error {
+	err := db.AutoMigrate(&Book{})
+	return err
 }
